688674cbf03cae92155f3f3a20de40078b7d1fa4
Arlan <104730663+HappyARL@users.noreply.github.com>
2023-03-01 15:23:47 +0600

    CMake files

---
+++ bin/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/arlan/documents/cpp_course/cpp_course22_aliyev_arlan_2022/calculator_cpp_yaishenka
+# Build directory: /Users/arlan/documents/cpp_course/cpp_course22_aliyev_arlan_2022/calculator_cpp_yaishenka/bin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(calculator_cpp_yaishenka "/bin/testing")
+set_tests_properties(calculator_cpp_yaishenka PROPERTIES  _BACKTRACE_TRIPLES "/Users/arlan/documents/cpp_course/cpp_course22_aliyev_arlan_2022/calculator_cpp_yaishenka/CMakeLists.txt;15;add_test;/Users/arlan/documents/cpp_course/cpp_course22_aliyev_arlan_2022/calculator_cpp_yaishenka/CMakeLists.txt;0;")
