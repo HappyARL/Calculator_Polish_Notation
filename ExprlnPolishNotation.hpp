eb60fef26c92fff15acd5128a6acc76a3d1c6bbb
Arlan <104730663+HappyARL@users.noreply.github.com>
2023-03-03 22:13:29 +0600

    Update ExprlnPolishNotation.hpp

--- ExprlnPolishNotation.hpp
+++ ExprlnPolishNotation.hpp
@@ -11,8 +11,6 @@
 #include "OperatorPLUS.hpp"
 #include "OperatorSUB.hpp"
 #include "OperatorToken.hpp"
-#include "OperatorUMINUS.hpp"
-#include "OperatorUPLUS.hpp"
 
 template <typename T>
 class ExprInPolishNotation {
@@ -20,9 +18,8 @@ class ExprInPolishNotation {
   std::vector<std::pair<AbstractToken*, int> > abstract_;
 
  public:
-  int Priority(OperatorToken<T>* oper) {
+  int Priority(std::string sign) {
     int prior;
-    std::string sign = oper->GetStringToken();
     if (sign == "+") {
       prior = 1;
     }
@@ -57,44 +54,91 @@ class ExprInPolishNotation {
     return std::make_pair(num, index);
   }
 
+  std::pair<std::pair<AbstractToken*, int>, bool> MakePLUSPair(bool is_binary) {
+    enum Index { Operand = 1, Bracket, BINOperator, UNAROperator };
+    if (is_binary) {
+      is_binary = false;
+      return std::make_pair(
+          std::make_pair(new OperatorPLUS<T, true>("+"), BINOperator),
+          is_binary);
+    }
+    return std::make_pair(
+        std::make_pair(new OperatorPLUS<T, false>("+"), UNAROperator),
+        is_binary);
+  }
+
+  std::pair<std::pair<AbstractToken*, int>, bool> MakeMINUSPair(
+      bool is_binary) {
+    enum Index { Operand = 1, Bracket, BINOperator, UNAROperator };
+    if (is_binary) {
+      is_binary = false;
+      return std::make_pair(
+          std::make_pair(new OperatorMINUS<T, true>("-"), BINOperator),
+          is_binary);
+    }
+    return std::make_pair(
+        std::make_pair(new OperatorMINUS<T, false>("~"), UNAROperator),
+        is_binary);
+  }
+
+  std::pair<std::pair<AbstractToken*, int>, bool> MakeMULPair(bool is_binary) {
+    enum Index { Operand = 1, Bracket, BINOperator, UNAROperator };
+    if (is_binary) {
+      is_binary = false;
+      return std::make_pair(
+          std::make_pair(new OperatorMUL<T, true>("*"), BINOperator),
+          is_binary);
+    }
+    return std::make_pair(
+        std::make_pair(new OperatorMUL<T, false>("*"), UNAROperator),
+        is_binary);
+  }
+
+  std::pair<std::pair<AbstractToken*, int>, bool> MakeSUBPair(bool is_binary) {
+    enum Index { Operand = 1, Bracket, BINOperator, UNAROperator };
+    if (is_binary) {
+      is_binary = false;
+      return std::make_pair(
+          std::make_pair(new OperatorSUB<T, true>("/"), BINOperator),
+          is_binary);
+    }
+    return std::make_pair(
+        std::make_pair(new OperatorSUB<T, false>("/"), UNAROperator),
+        is_binary);
+  }
+
   std::vector<std::pair<AbstractToken*, int> > NewVector(
       const std::string& str) {
-    enum Index { Operand = 1, Bracket, Operator };
+    enum Index { Operand = 1, Bracket, BINOperator, UNAROperator };
     bool is_binary = false;
     std::vector<std::pair<AbstractToken*, int> > tmp;
     for (size_t i = 0; i < str.length(); ++i) {
       if (str[i] == '+') {
-        if (is_binary) {
-          tmp.push_back(
-              std::make_pair(new OperatorPLUS<T>("+", is_binary), Operator));
-          is_binary = false;
-        } else {
-          tmp.push_back(
-              std::make_pair(new OperatorUPLUS<T>("+", is_binary), Operator));
-        }
+        std::pair<std::pair<AbstractToken*, int>, bool> plus_pair =
+            MakePLUSPair(is_binary);
+        tmp.push_back(plus_pair.first);
+        is_binary = plus_pair.second;
         continue;
       }
       if (str[i] == '-') {
-        if (is_binary) {
-          tmp.push_back(
-              std::make_pair(new OperatorMINUS<T>("-", is_binary), Operator));
-          is_binary = false;
-        } else {
-          tmp.push_back(
-              std::make_pair(new OperatorUMINUS<T>("~", is_binary), Operator));
-        }
+        std::pair<std::pair<AbstractToken*, int>, bool> minus_pair =
+            MakeMINUSPair(is_binary);
+        tmp.push_back(minus_pair.first);
+        is_binary = minus_pair.second;
         continue;
       }
       if (str[i] == '*') {
-        tmp.push_back(
-            std::make_pair(new OperatorMUL<T>("*", is_binary), Operator));
-        is_binary = false;
+        std::pair<std::pair<AbstractToken*, int>, bool> mul_pair =
+            MakeMULPair(is_binary);
+        tmp.push_back(mul_pair.first);
+        is_binary = mul_pair.second;
         continue;
       }
       if (str[i] == '/') {
-        tmp.push_back(
-            std::make_pair(new OperatorSUB<T>("/", is_binary), Operator));
-        is_binary = false;
+        std::pair<std::pair<AbstractToken*, int>, bool> sub_pair =
+            MakeSUBPair(is_binary);
+        tmp.push_back(sub_pair.first);
+        is_binary = sub_pair.second;
         continue;
       }
       if (str[i] == '(') {
@@ -128,7 +172,7 @@ class ExprInPolishNotation {
   }
 
   ExprInPolishNotation(const std::string& str) {
-    enum Index { Operand = 1, Bracket, Operator };
+    enum Index { Operand = 1, Bracket, BINOperator, UNAROperator };
     std::vector<std::pair<AbstractToken*, int> > tmp = NewVector(str);
     std::stack<std::pair<AbstractToken*, int> > stack;
     for (size_t i = 0; i < tmp.size(); ++i) {
@@ -160,15 +204,13 @@ class ExprInPolishNotation {
         }
         continue;
       }
-      OperatorToken<T>* operator_token =
-          static_cast<OperatorToken<T>*>(tmp[i].first);
+      std::string operator_token = tmp[i].first->GetStringToken();
       while (!stack.empty()) {
         if (stack.top().second == Bracket) {
           break;
         }
-        OperatorToken<T>* oper_stack =
-            static_cast<OperatorToken<T>*>(stack.top().first);
-        if (Priority(oper_stack) > Priority(operator_token)) {
+        std::string operator_stack = stack.top().first->GetStringToken();
+        if (Priority(operator_stack) > Priority(operator_token)) {
           abstract_.push_back(stack.top());
           stack.pop();
         }
