aa6c66f63962377ea51c145775345500a947d4b0
Arlan <104730663+HappyARL@users.noreply.github.com>
2023-03-03 22:11:54 +0600

    Update OperatorPLUS.hpp

--- OperatorPLUS.hpp
+++ OperatorPLUS.hpp
@@ -3,12 +3,11 @@
 #include "OperandToken.hpp"
 #include "OperatorToken.hpp"
 
-template <typename T>
-class OperatorPLUS : public OperatorToken<T> {
+template <typename T, bool Binary>
+class OperatorPLUS : public OperatorToken<T, Binary> {
  private:
  public:
-  OperatorPLUS(const std::string& str, bool binary)
-      : OperatorToken<T>(str, binary) {}
+  OperatorPLUS(const std::string& str) : OperatorToken<T, Binary>(str) {}
 
   OperandToken<T>* Calculate(OperandToken<T>* lhs,
                              OperandToken<T>* rhs) override {
@@ -19,6 +18,10 @@ class OperatorPLUS : public OperatorToken<T> {
   }
 
   OperandToken<T>* Calculate(OperandToken<T>* operand) override {
-    return operand;
+    T positive_operand = operand->GetValue();
+    positive_operand =
+        positive_operand * ((positive_operand > 0) - (positive_operand < 0));
+    OperandToken<T>* result = new OperandToken<T>(positive_operand);
+    return result;
   }
 };
